0508ed64c37ed64eb2e828a9bafdc73eb30fdce8
Ogbonna Patrick <53985539+Fitzpatrique@users.noreply.github.com>
2023-04-03 20:30:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./weather","./index.css","./images/weather-app-icon.png","./images/search.png"]);
+            return cache.addAll(["./","./index.css","./images/weather-app-icon.png","./images/search.png"]);
         })
     );
 });
